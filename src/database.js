ced91906a14b0876c81343137d70af3f93395630
Naymco <naymco2013@gmail.com>
2019-08-25 19:59:01 +0200

    version 1.2.3

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/notes-db-app', {
+mongoose.connect('mongodb+srv://naymco:D%r3%a%m%w%3av%3r%@@cluster0-vamxx.mongodb.net/test?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
